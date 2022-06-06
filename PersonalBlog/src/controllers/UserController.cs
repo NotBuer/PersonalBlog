4add70de525be1f3cf595fd0034efa1f538f1beb
NotBuer <rodrigosoocer@gmail.com>
2022-06-06 13:04:27 -0300

    UserController.cs fix

--- PersonalBlog/src/controllers/UserController.cs
+++ PersonalBlog/src/controllers/UserController.cs
@@ -61,7 +61,7 @@ namespace PersonalBlog.src.controllers
         /// <response code="404">User does not exist</response>
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UsersModel))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [HttpGet("name/{nameUser}")]
+        [HttpGet]
         [Authorize(Roles = "NORMAL,ADMIN")]
         public async Task<ActionResult> GetUsersByNameAsync([FromQuery] string name)
         {
@@ -101,7 +101,7 @@ namespace PersonalBlog.src.controllers
         [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(UsersModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [HttpPost("create")]
+        [HttpPost]
         [AllowAnonymous]
         public async Task<ActionResult> NewUserAsync([FromBody] NewUserDTO userDTO)
         {
